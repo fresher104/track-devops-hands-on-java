3be3af057089cee34dcf41078b09d0d95e233d7c
fresher104 <jiahong.chen@tcs.com>
2026-06-12 13:44:47 +0900

    Update Calculator.java

--- src/main/java/com/example/handson/Calculator.java
+++ src/main/java/com/example/handson/Calculator.java
@@ -1,9 +1,24 @@
 package com.example.handson;
+
 public class Calculator {
- public double add(double a, double b) {
- return a + b;
- }
- public double add(double a, double b, double c) {
- return a + b + c;
- }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double add(double a, double b, double c) {
+        validateRange(a);
+        validateRange(b);
+        validateRange(c);
+        return a + b + c;
+    }
+
+    private void validateRange(double value) {
+        if (value < 0 || value > 10) {
+            throw new IllegalArgumentException("value must be between 0 and 10");
+        }
+    }
+
 }
+
+
